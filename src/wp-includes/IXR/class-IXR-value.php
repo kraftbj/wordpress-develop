55775db51139a07e4da0e07f68f015b50968ff87
Brandon Kraft <public@brandonkraft.com>
2020-06-30 15:57:15 -0500

    Remove unreachable code

--- src/wp-includes/IXR/class-IXR-value.php
+++ src/wp-includes/IXR/class-IXR-value.php
@@ -79,16 +79,12 @@ class IXR_Value {
 		switch ( $this->type ) {
 			case 'boolean':
 				return '<boolean>' . ( ( $this->data ) ? '1' : '0' ) . '</boolean>';
-				break;
 			case 'int':
 				return '<int>' . $this->data . '</int>';
-				break;
 			case 'double':
 				return '<double>' . $this->data . '</double>';
-				break;
 			case 'string':
 				return '<string>' . htmlspecialchars( $this->data ) . '</string>';
-				break;
 			case 'array':
 				$return = '<array><data>' . "\n";
 				foreach ( $this->data as $item ) {
@@ -96,7 +92,6 @@ class IXR_Value {
 				}
 				$return .= '</data></array>';
 				return $return;
-				break;
 			case 'struct':
 				$return = '<struct>' . "\n";
 				foreach ( $this->data as $name => $value ) {
@@ -106,11 +101,9 @@ class IXR_Value {
 				}
 				$return .= '</struct>';
 				return $return;
-				break;
 			case 'date':
 			case 'base64':
 				return $this->data->getXml();
-				break;
 		}
 		return false;
 	}
